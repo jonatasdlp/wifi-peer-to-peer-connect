a3e0bb5437540a5a0d265d7a6124d91e0bfc7947
Jônatas Santos <jonatas.dlp@gmail.com>
2016-03-03 09:32:27 -0300

    initial commit

---
+++ app/src/main/java/com/pucminas/tcc/jonatas/wifip2pdbsync/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pucminas.tcc.jonatas.wifip2pdbsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
